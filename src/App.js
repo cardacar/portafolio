557e76575163c5a48c9b5b46f27da5f4d5c411d8
Carlos Quiros <carlosquiros12@gmail.com>
2021-03-16 10:52:03 -0500

    Limpiando el proyecto y agregando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <h1>Hola mundo</h1>
   );
 }
 
